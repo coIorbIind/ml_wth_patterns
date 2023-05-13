5f1ea0fcdd5174dd697fa1f1e9d032fadf9b6eae
colorblind <colorblind.backend.work@gmail.com>
2023-05-13 11:09:21 +0300

    add frontend

--- src/ds_model_with_fastapi/frontend/js/script.js
+++ src/ds_model_with_fastapi/frontend/js/script.js
@@ -1,91 +1,34 @@
 jQuery(document).ready(function ($) {
-	var maxFileSize = 10 * 1024 * 1024 ; // (байт) Максимальный размер файла (2мб)
-	var queue = {};
-	var form = $('form#uploadImages');
-	var imagesList = $('#uploadImagesList');
+    $('#my_image').change(function(){
+        const file = this.files[0];
+        console.log(file);
+        if (file){
+          let reader = new FileReader();
+          reader.onload = function(event){
+            $('#my_image_preview').attr('src', event.target.result);
+          }
+          reader.readAsDataURL(file);
+        }
+      });
 
-	var itemPreviewTemplate = imagesList.find('.item.template').clone();
-	itemPreviewTemplate.removeClass('template');
-	imagesList.find('.item.template').remove();
+    $(function () {
+        $('input[type=submit]').click(function (event) {
+            event.preventDefault();
 
+            var formData = new FormData();
+            formData.append('file', $("#my_image")[0].files[0]);
 
-	$('#addImages').on('change', function () {
-			var files = this.files;
-
-			for (var i = 0; i < files.length; i++) {
-					var file = files[i];
-
-					if ( !file.type.match(/image\/(jpeg|jpg|png|gif)/) ) {
-							alert( 'Фотография должна быть в формате jpg, png или gif' );
-							continue;
-					}
-
-					if ( file.size > maxFileSize ) {
-							alert( 'Размер фотографии не должен превышать 2 Мб' );
-							continue;
-					}
-
-					preview(files[i]);
-			}
-
-			this.value = '';
-	});
-
-	// Создание превью
-	function preview(file) {
-			var reader = new FileReader();
-			reader.addEventListener('load', function(event) {
-					var img = document.createElement('img');
-
-					var itemPreview = itemPreviewTemplate.clone();
-
-					itemPreview.find('.img-wrap img').attr('src', event.target.result);
-					itemPreview.data('id', file.name);
-					/* imagesList.style.width='200px';
-					imagesList.style.height='200px'; */
-
-					imagesList.append(itemPreview);
-
-
-					queue[file.name] = file;
-
-			});
-			reader.readAsDataURL(file);
-	}
-
-	// Удаление фотографий
-	imagesList.on('click', '.delete-link', function () {
-			var item = $(this).closest('.item'),
-					id = item.data('id');
-
-			delete queue[id];
-
-			item.remove();
-	});
-
-
-	// Отправка формы
-	form.on('submit', function(event) {
-
-			var formData = new FormData(this);
-
-			for (var id in queue) {
-					formData.append('images[]', queue[id]);
-			}
-
-			$.ajax({
-					url: $(this).attr('action'),
-					type: 'POST',
-					data: formData,
-					async: true,
-					success: function (res) {
-							alert(res)
-					},
-					cache: false,
-					contentType: false,
-					processData: false
-			});
-
-			return false;
-	});
+            // send request
+            $.ajax({
+                type: 'POST',
+                url: 'http://127.0.0.1:8000/api/v1/model/predict',
+                data: formData,
+                processData: false,
+                contentType: false,
+                success: function (msg) {
+                    $('#prediction').text(`Это ${msg.prediction}`);
+                }
+            });
+        });
+    });
 });
